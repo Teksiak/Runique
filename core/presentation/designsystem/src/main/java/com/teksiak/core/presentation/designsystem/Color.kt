a1dc2383078381c43a8bc87ff804c282dfb28338
Teksiak <bednarowski.wojciech@gmail.com>
2024-04-30 23:38:03 +0200

    feat: Add colors, themes and fonts to design system.

---
+++ core/presentation/designsystem/src/main/java/com/teksiak/core/presentation/designsystem/Color.kt
@@ -0,0 +1,20 @@
+package com.teksiak.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val RuniqueBlue = Color(0xFF0D85D8)
+val RuniqueBlue30 = Color(0x4D0D85D8)
+val RuniqueBlue10 = Color(0x1A0D85D8)
+val RuniqueBlue5 = Color(0x0D0D85D8)
+val RuniqueGreen = Color(0xFF00F15E)
+val RuniqueGreen30 = Color(0x4D00F15E)
+val RuniqueGreen10 = Color(0x1A00F15E)
+val RuniqueGreen5 = Color(0x0D00F15E)
+val RuniqueBlack = Color(0xFF080707)
+val RuniqueDarkGray = Color(0xFF232624)
+val RuniqueGray = Color(0xFF87938C)
+val RuniqueGray40 = Color(0x6687938C)
+val RuniqueGray5 = Color(0x0D87938C)
+val RuniqueWhite = Color(0xFFFAFAFA)
+val RuniqueDarkRed = Color(0xFFBB3D3D)
+val RuniqueDarkRed5 = Color(0x0DBB3D3D)
---
+++ core/presentation/designsystem/src/main/java/com/teksiak/core/presentation/designsystem/Theme.kt
@@ -0,0 +1,60 @@
+package com.teksiak.core.presentation.designsystem
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+val DarkColorScheme = darkColorScheme(
+    primary = RuniqueGreen,
+    background = RuniqueBlack,
+    surface = RuniqueDarkGray,
+    secondary = RuniqueWhite,
+    tertiary = RuniqueWhite,
+    primaryContainer = RuniqueGreen30,
+    onPrimary = RuniqueBlack,
+    onBackground = RuniqueWhite,
+    onSurface = RuniqueWhite,
+    onSurfaceVariant = RuniqueGray,
+)
+
+val LightColorScheme = darkColorScheme(
+    primary = RuniqueBlue,
+    background = RuniqueWhite,
+    surface = RuniqueGray5,
+    secondary = RuniqueBlack,
+    tertiary = RuniqueBlack,
+    primaryContainer = RuniqueBlue30,
+    onPrimary = RuniqueWhite,
+    onBackground = RuniqueBlack,
+    onSurface = RuniqueBlack,
+    onSurfaceVariant = RuniqueGray,
+)
+
+@Composable
+fun RuniqueTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
