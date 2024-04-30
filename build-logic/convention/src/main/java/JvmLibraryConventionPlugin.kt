a98c22570219e9872dce5f689036923c8a907bb3
Teksiak <bednarowski.wojciech@gmail.com>
2024-04-30 18:06:20 +0200

    chore: Add JvmKtorConventionPlugin.

--- build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
@@ -1,13 +1,6 @@
-import com.android.build.api.dsl.LibraryExtension
-import com.teksiak.convention.ExtensionType
-import com.teksiak.convention.configureBuildTypes
-import com.teksiak.convention.configureKotlinAndroid
 import com.teksiak.convention.configureKotlinJvm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.kotlin
 
 class JvmLibraryConventionPlugin: Plugin<Project> {
 
