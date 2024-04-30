dd82e9d1b9a2208a622b79758ab74de895cc3f64
Teksiak <bednarowski.wojciech@gmail.com>
2024-04-30 17:22:34 +0200

    chore: Add AndroidRoomConventionPlugin.

---
+++ build-logic/convention/src/main/java/AndroidRoomConventionPlugin.kt
@@ -0,0 +1,36 @@
+import androidx.room.gradle.RoomExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.teksiak.convention.ExtensionType
+import com.teksiak.convention.configureAndroidCompose
+import com.teksiak.convention.configureBuildTypes
+import com.teksiak.convention.configureKotlinAndroid
+import com.teksiak.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidRoomConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("androidx.room")
+                apply("com.google.devtools.ksp")
+            }
+
+            extensions.configure<RoomExtension> {
+                schemaDirectory("$projectDir/schemas")
+            }
+
+            dependencies {
+                "implementation"(libs.findLibrary("room.runtime").get())
+                "implementation"(libs.findLibrary("room.ktx").get())
+                "ksp"(libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+
+}
