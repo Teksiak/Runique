3a8ebaefba545d21bf826cf7357d4323b377c4e0
Teksiak <bednarowski.wojciech@gmail.com>
2024-05-06 22:21:00 +0200

    feat: Add Google Maps to ActiveRun screen.

--- run/presentation/src/main/java/com/teksiak/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/teksiak/run/presentation/active_run/ActiveRunScreen.kt
@@ -32,6 +32,7 @@ import com.teksiak.core.presentation.designsystem.components.RuniqueScaffold
 import com.teksiak.core.presentation.designsystem.components.RuniqueToolbar
 import com.teksiak.run.presentation.R
 import com.teksiak.run.presentation.active_run.components.RunDataCard
+import com.teksiak.run.presentation.active_run.maps.TrackerMap
 import com.teksiak.run.presentation.util.hasLocationPermission
 import com.teksiak.run.presentation.util.hasNotificationPermission
 import com.teksiak.run.presentation.util.shouldShowLocationPermissionRationale
@@ -141,6 +142,14 @@ private fun ActiveRunScreen(
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.surface)
         ) {
+            TrackerMap(
+                isRunFinished = state.isRunFinished,
+                currentLocation = state.currentLocation,
+                locations = state.runData.locations,
+                onSnapshot = { },
+                modifier = Modifier
+                    .fillMaxSize()
+            )
             RunDataCard(
                 elapsedTime = state.elapsedTime,
                 runData = state.runData,
