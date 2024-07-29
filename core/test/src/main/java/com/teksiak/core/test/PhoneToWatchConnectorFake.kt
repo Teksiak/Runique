9b89f7a95fe7880e3407fcdd04e61e062eed338b
Teksiak <bednarowski.wojciech@gmail.com>
2024-07-29 20:34:44 +0200

    test: Set up test utility module.

---
+++ core/test/src/main/java/com/teksiak/core/test/PhoneToWatchConnectorFake.kt
@@ -0,0 +1,48 @@
+package com.teksiak.core.test
+
+import com.teksiak.core.connectivity.domain.DeviceNode
+import com.teksiak.core.connectivity.domain.messaging.MessagingAction
+import com.teksiak.core.connectivity.domain.messaging.MessagingError
+import com.teksiak.core.domain.util.EmptyResult
+import com.teksiak.core.domain.util.Result
+import com.teksiak.run.domain.WatchConnector
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PhoneToWatchConnectorFake: WatchConnector {
+
+    var sendError: MessagingError? = null
+
+    private val _isTrackable = MutableStateFlow(true)
+
+    private val _connectedDevice = MutableStateFlow<DeviceNode?>(null)
+
+    override val connectedDevice: StateFlow<DeviceNode?>
+        get() = _connectedDevice.asStateFlow()
+
+    private val _messagingActions = MutableSharedFlow<MessagingAction>()
+
+    override val messagingActions: Flow<MessagingAction>
+        get() = _messagingActions.asSharedFlow()
+
+    override suspend fun sendActionToWatch(action: MessagingAction): EmptyResult<MessagingError> {
+        return if(sendError != null) {
+            Result.Success(Unit)
+        } else {
+            Result.Error(sendError!!)
+
+        }
+    }
+
+    override fun setIsTrackable(isTrackable: Boolean) {
+        _isTrackable.value = isTrackable
+    }
+
+    suspend fun sendFromWatchToPhone(action: MessagingAction) {
+        _messagingActions.emit(action)
+    }
+}
